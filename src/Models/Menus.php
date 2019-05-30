5694e0cb522e36272126783a56ff7d75935a13aa
Justin Slabbert <justin@novarand.com>
2019-05-30 20:34:45 +0200

    Update Menus.php

--- src/Models/Menus.php
+++ src/Models/Menus.php
@@ -2,10 +2,12 @@
 
 namespace Harimayco\Menu\Models;
 
+use Hyn\Tenancy\Traits\UsesTenantConnection;
 use Illuminate\Database\Eloquent\Model;
 
 class Menus extends Model
 {
+    use UsesTenantConnection;
     protected $table = 'menus';
 
     public function __construct( array $attributes = [] ){
